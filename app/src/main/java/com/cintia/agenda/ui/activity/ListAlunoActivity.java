54fbcd44b3dc653d2af3d918af20e0e4c8b5d729
Cintia Ribeiro <cintiarbrandao@gmail.com>
2022-06-22 22:00:45 -0300

    Refatorando e criando activity de formulario de cadastro

--- app/src/main/java/com/cintia/agenda/ui/activity/ListAlunoActivity.java
+++ app/src/main/java/com/cintia/agenda/ui/activity/ListAlunoActivity.java
@@ -1,6 +1,5 @@
-package com.cintia.agenda;
+package com.cintia.agenda.ui.activity;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -8,19 +7,21 @@ import android.widget.ListView;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.cintia.agenda.R;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class ListAlunoActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setTitle("Lista de Alunos");
-        setContentView(R.layout.main_activity);
+        setContentView(R.layout.activity_list_aluno);
 
         List<String> alunos = new ArrayList<String>(Arrays.asList("Lucca", "Léo", "Luizinho"));
-        ListView listAlunos = findViewById(R.id.activity_main_fab_novo_aluno);
+        ListView listAlunos = findViewById(R.id.activity_lista_alunos_list_view);
         listAlunos.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, alunos));
 
     }
