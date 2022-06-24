2b0cd773e6f1f00fb0f8d6b4874363ace2239ed2
Cintia Ribeiro <cintiarbrandao@gmail.com>
2022-06-23 23:17:56 -0300

    criando model de aluno

--- app/src/main/java/com/cintia/agenda/ui/activity/FormularioAlunoActivity.java
+++ app/src/main/java/com/cintia/agenda/ui/activity/FormularioAlunoActivity.java
@@ -3,8 +3,13 @@ package com.cintia.agenda.ui.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.cintia.agenda.R;
+import com.cintia.agenda.model.Aluno;
 
 public class FormularioAlunoActivity extends AppCompatActivity {
 
@@ -12,5 +17,26 @@ public class FormularioAlunoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario_aluno);
+
+        final EditText campoNome = findViewById(R.id.activity_formulario_aluno_nome);
+        final EditText campoTelefone = findViewById(R.id.activity_formulario_aluno_telefone);
+        final EditText campoEmail = findViewById(R.id.activity_formulario_aluno_email);
+
+        Button btnSalvar = findViewById(R.id.activity_formulario_aluno_btn_salvar);
+
+        btnSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String nome = campoNome.getText().toString();
+                String telefone = campoTelefone.getText().toString();
+                String email = campoEmail.getText().toString();
+
+                Aluno alunoCriado = new Aluno(nome, telefone, email);
+
+                Toast.makeText(FormularioAlunoActivity.this,
+                        alunoCriado.getNome() + '_'+ alunoCriado.getTelefone() +'_'+ alunoCriado.getEmail(),
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
